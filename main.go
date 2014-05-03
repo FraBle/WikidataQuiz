eda464baaed6db52566042e96c6e9d0c765d64eb
Frank Blechschmidt <frable90@gmail.com>
2014-05-03 16:34:09 +0200

    correct static path

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 
 	// router.NotFoundHandler = http.HandlerFunc(handler.NotFound)
 	router.HandleFunc("/", handler.HomeHandler).Methods("GET")
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../src/github.com/FraBle/WikidataQuiz/static"))))
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
