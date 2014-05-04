d05354a76f4a5fbdfa578acfecf8da1baae46412
peterHeuz <jheuz@web.de>
2014-05-04 11:50:36 +0200

    completed CapitalQuestions

--- question/places.go
+++
@@ -1,9 +0,0 @@
-package question
-
-import (
-	"github.com/FraBle/WikidataQuiz/model"
-)
-
-func CapitalQuestion() *model.Question {
-	return &model.Question{"Ich bin eine Beispielfrage?", []string{"Antwort1", "Antwort2", "Antwort3", "Antwort4"}, 2}
-}
